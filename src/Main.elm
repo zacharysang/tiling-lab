76d28b336711ed24c0e095c52da7c2a753f215e2
zacharysang <zacharysang@gmail.com>
2019-08-11 06:05:24 +0000

    Use elm element instead of sandbox
    * Allow using custom css by building to public instead of using elm
    reactor
    * Setup npm project with 'watch' target for rebuilding
    * Main.elm does not contain state-independent styles

--- src/Main.elm
+++ src/Main.elm
@@ -1,49 +1,59 @@
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, text, p)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
 
-main =
-  Browser.sandbox { init = init, update = update, view = view }
+main = Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
+-- SUBSCRIPTIONS (unused)
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
 
 -- MODEL
 
 type alias Model = {
-  tile: Shape,
-  textHeight: Int,
+  tile: List Float,
   textWidth: Int }
-type alias Shape = List Float -- represent a shape as a list of angles
 
-init : Model
-init = {
-  tile = [1,2],
-  textHeight = 200,
-  textWidth = 200 }
+init : () -> (Model, Cmd Msg)
+init _ = (
+    {
+      tile = [],
+      textWidth = 300
+    },
+    Cmd.none
+  )
 
 
 -- UPDATE
 
-type Msg = SetTile Shape | SetTextHeight Float | SetTextWidth Float
+type Msg = SetTile (List Float) | SetTextWidth Float
 
 -- update will take a Model (the current one), and a new Shape, and output the new Shape
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg currModel = 
   case msg of
-    SetTile tile -> currModel
-    SetTextHeight height -> currModel
-    SetTextWidth width -> currModel
+    SetTile tile -> (currModel, Cmd.none)
+    SetTextWidth width -> (currModel, Cmd.none)
   
-
 
 -- VIEW
 
 view : Model -> Html msg -- right now message is a dummy since the produced html never emits messages
 view model =
-  div []
-    [ div [ class "col1", width model.textWidth] [
-        div [class "row1", height model.textHeight, style "background-color" "red"] [text (String.fromInt (List.length model.tile)) ],
-        div [class "row2", style "background-color" "blue"] []
+  div [class "container"]
+    [ 
+     div [ class "sidebar",
+            style "width" ((String.fromInt model.textWidth) ++ "px")]
+      [
+        div [ class "source" ] [],
+        div [ class "horizontal-gutter" ] [],
+        div [ class "source" ] []
       ],
-      div [ class "col2" ] []]
+      div [class "plane"] [
+        p [] [text ("model.textWidth: " ++ (String.fromInt model.textWidth))],
+        p [] [text ("model.tile: " ++ String.join "," (List.map String.fromFloat model.tile))]
+      ]
+    ]
